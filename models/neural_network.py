e3e658e5dc0495d17721a49877b38cbd08acb19d
Ipsedo <berriens@gmail.com>
2019-02-06 15:25:34 +0100

    add conv classifier

--- models/neural_network.py
+++ models/neural_network.py
@@ -55,3 +55,22 @@ class LinearClassifier(nn.Module):
             self.lin.weight.retain_grad()
             out = self.lin(x)
             return self.sig(out)
+
+# shape pour MNIST /!\
+class ConvolutionClassifier(nn.Module):
+    def __init__(self):
+        super(ConvolutionClassifier, self).__init__()
+        self.seq = nn.Sequential(nn.Conv2d(1, 8, kernel_size=(3, 3)),
+                                 nn.MaxPool2d((3, 3)),
+                                 nn.ReLU(),
+                                 nn.Conv2d(8, 16, kernel_size=(3, 3)),
+                                 nn.MaxPool2d((5, 5)),
+                                 nn.ReLU())
+
+        self.lin = nn.Linear(16, 1)
+        self.sig = nn.Sigmoid()
+
+    def forward(self, x):
+        out = self.seq(x)
+        out = self.lin(out)
+        return self.sig(out)
