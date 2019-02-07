e1d720ef3ebc52c6b8bde3701a0b605f7c02dae0
Ipsedo <berriens@gmail.com>
2019-02-07 11:44:07 +0100

    add conv model for cifar10

--- models/neural_network.py
+++ models/neural_network.py
@@ -71,3 +71,32 @@ class ConvolutionClassifier(nn.Module):
         out = self.seq(x).view(-1, 8 * 8 * 8)
         out = self.lin(out)
         return self.sig(out)
+
+# shape pour cifar10 /!\
+class ConvModel(nn.Module):
+
+    def __init__(self):
+        img_size = 32
+        super(ConvModel, self).__init__()
+
+        # conv1 : img 3 * 32 * 32 -> img 20 * 28 * 28
+        # maxpool1 : img 20 * 28 * 28 -> img 20 * 14 * 14
+        # conv2 : img 20 * 14 * 14 -> img 50 * 10 * 10
+        # maxpool2 : img 50 * 10 * 10 -> 50 * 5 * 5
+
+        self.seq = nn.Sequential(nn.Conv2d(3, 20, (5,5)),
+                                 nn.MaxPool2d((2, 2), stride=(2, 2)),
+                                 nn.ReLU(),
+                                 nn.Conv2d(20, 50, (5, 5)),
+                                 nn.MaxPool2d((2, 2), stride=(2, 2)),
+                                 nn.ReLU())
+
+        self.linear1_dim = int((((img_size - 4) / 2 - 4) / 2) ** 2 * 50)
+        self.lin = nn.Linear(self.linear1_dim, 1)
+        self.sig = nn.Sigmoid()
+
+    def forward(self, x):
+        out = self.seq(x).view(-1, self.linear1_dim)
+        out = self.lin(out)
+        out = self.sig(out)
+        return out
