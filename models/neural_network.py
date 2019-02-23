ad348d864fa743000272d2cbca5946392903b5b5
Ipsedo <berriens@gmail.com>
2019-02-23 13:36:25 +0100

    add multiclass pytorch model (à utiliser ?)

--- models/neural_network.py
+++ models/neural_network.py
@@ -74,7 +74,6 @@ class ConvolutionClassifier(nn.Module):
 
 # shape pour cifar10 /!\
 class ConvModel(nn.Module):
-
     def __init__(self):
         img_size = 32
         super(ConvModel, self).__init__()
@@ -99,4 +98,30 @@ class ConvModel(nn.Module):
         out = self.seq(x).view(-1, self.linear1_dim)
         out = self.lin(out)
         out = self.sig(out)
+        return out
+
+
+class ConvModelMultiClass(nn.Module):
+    def __init__(self, nb_class):
+        img_size = 32
+        super(ConvModelMultiClass, self).__init__()
+
+        # conv1 : img 3 * 32 * 32 -> img 20 * 28 * 28
+        # maxpool1 : img 20 * 28 * 28 -> img 20 * 14 * 14
+        # conv2 : img 20 * 14 * 14 -> img 50 * 10 * 10
+        # maxpool2 : img 50 * 10 * 10 -> 50 * 5 * 5
+
+        self.seq = nn.Sequential(nn.Conv2d(3, 20, (5,5)),
+                                 nn.MaxPool2d((2, 2), stride=(2, 2)),
+                                 nn.ReLU(),
+                                 nn.Conv2d(20, 50, (5, 5)),
+                                 nn.MaxPool2d((2, 2), stride=(2, 2)),
+                                 nn.ReLU())
+
+        self.linear1_dim = int((((img_size - 4) / 2 - 4) / 2) ** 2 * 50)
+        self.lin = nn.Linear(self.linear1_dim, nb_class)
+
+    def forward(self, x):
+        out = self.seq(x).view(-1, self.linear1_dim)
+        out = self.lin(out)
         return out
