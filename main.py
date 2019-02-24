f843397a2ed6285459197e415030c71dbcf512dd
Ipsedo <berriens@gmail.com>
2019-02-24 21:11:49 +0100

    add new arg main : teacher type "omni" or "surro"

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ import sys
 if __name__ == "__main__":
 
     if sys.argv[1] == "gaussian":
-        gaussian_data.gaussian_main()
+        if sys.argv[2] == "omni":
+            gaussian_data.gaussian_omniscient_main()
+        elif sys.argv[2] == "surro":
+            gaussian_data.gaussian_surrogate_main()
     elif sys.argv[1] == "mnist":
-        mnist.mnist_data()
+        if sys.argv[2] == "omni":
+            mnist.mnist_data_omniscient()
+        elif sys.argv[2] == "surro":
+            mnist.mnist_data_surrogate()
     elif sys.argv[1] == "cifar":
         cifar10.cifar10_main()
 
