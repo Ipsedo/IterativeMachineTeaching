0018e36b47534d09442e5915f88bd515f4925f00
Ipsedo <berriens@gmail.com>
2019-02-26 23:56:10 +0100

    fix learning rate

--- experiences/cifar10.py
+++ experiences/cifar10.py
@@ -38,7 +38,7 @@ def cifar10_main(teacher_type):
     print(data.shape)
     print(labels.shape)
 
-    eta = 3e-4
+    eta = 1e-3
 
     if teacher_type == "omni":
         teacher = omni.OmniscientConvTeacher(eta)
